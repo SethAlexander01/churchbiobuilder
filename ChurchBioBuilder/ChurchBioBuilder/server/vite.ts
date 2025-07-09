4ba3a5ae0a0b68909bbb12b292e69a07fc13a081
SethAlexander01 <xander170612@gmail.com>
2025-07-08 21:19:00 -0400

    Update vite.ts

--- ChurchBioBuilder/ChurchBioBuilder/server/vite.ts
+++ ChurchBioBuilder/ChurchBioBuilder/server/vite.ts
@@ -72,7 +72,7 @@ export async function setupVite(app: Express, server: Server) {
 }
 
 export function serveStatic(app: Express) {
-  const distPath = path.resolve(__dirname, "public");  // <--- changed here
+  const distPath = path.resolve(__dirname, "..", "client", "dist");
 
   if (!fs.existsSync(distPath)) {
     throw new Error(
@@ -82,7 +82,6 @@ export function serveStatic(app: Express) {
 
   app.use(express.static(distPath));
 
-  // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
