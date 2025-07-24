ea177b449a64dda28bdc7c92ec54373c7eb29118
Seth Alexander <xander170612@gmail.com>
2025-07-24 15:36:32 -0400

    Your descriptive update message

--- node_modules/client/tailwind.config.ts
+++ node_modules/client/tailwind.config.ts
@@ -3,8 +3,10 @@ import type { Config } from "tailwindcss"
 const config: Config = {
     darkMode: ["class"],
     content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+  "./index.html",
+  "./src/**/*.{js,ts,jsx,tsx}",
+  "../**/*.{js,ts,jsx,tsx}"  // <-- Add this!
+
   ],
   theme: {
   	extend: {
